419c3c4639e3dfa23f830aed8a4c3d6d1782ad91
Gilles Crofils <gilles.crofils@gmail.com>
2010-06-24 01:34:11 +0200

    work in progress

--- script/install.rb
+++ script/install.rb
@@ -13,3 +13,4 @@ themePath = File.expand_path(File.join(File.dirname(File.join(Dir.getwd, __FILE_
 targetPath = File.expand_path(File.join(wwwroot, magentoCurrent))
 
 FileUtils.cp_r File.join(themePath, appPath), targetPath
+FileUtils.cp_r File.join(skinPath, appPath), targetPath
