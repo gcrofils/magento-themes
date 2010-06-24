f2871f9b46db9d06f279fd47416fc07817966703
Gilles Crofils <gilles.crofils@gmail.com>
2010-06-25 01:56:27 +0200

    debug

--- script/install.rb
+++ script/install.rb
@@ -27,7 +27,7 @@ blocksPath = File.expand_path(File.join(themePath, 'blocks'))
 
 def execSql(sql)
  cmd = "mysql -u#{MYSQL_USER} -p#{MYSQL_PASSWORD} -e \"connect #{MYSQL_DATABASE}; #{sql.gsub('"','\"')}; \""
- puts "#{cmd}" 
+ system "#{cmd}" 
 end
 
 FileUtils.cp_r File.join(themePath, appPath), targetPath
@@ -48,7 +48,7 @@ Dir["#{pagesPath}/*"].select { |file| /(yml)$/ =~ file }.each do |file|
   queries << "delete from cms_page where identifier='#{identifier}'"
   params = YAML.load_file( file )
   params['identifier'] = identifier
-  queries << "insert into cms_page (#{params.keys.join(',')}) values ('#{params.values.join('\',\'')}')"
+  queries << "insert into cms_page (#{params.keys.join(',')}) values ('#{params.values.gsub('\'','\\\'').join('\',\'')}')"
 end
 
 queries.each{|q| execSql(q)}
