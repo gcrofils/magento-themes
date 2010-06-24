7c46193ba9d654caa6a3e226b18fc3bb0aee72d6
Gilles Crofils <gilles.crofils@gmail.com>
2010-06-24 23:24:01 +0200

    work in progress

--- script/install.rb
+++ script/install.rb
@@ -2,6 +2,8 @@
 
 require 'ftools'
 require 'fileutils'
+require 'yaml'
+
 
 magentoTheme = "webandpeople-fashion"
 wwwroot = "/home/www"
@@ -11,6 +13,13 @@ skinPath = "skin"
 
 themePath = File.expand_path(File.join(File.dirname(File.join(Dir.getwd, __FILE__)), '..', magentoTheme))
 targetPath = File.expand_path(File.join(wwwroot, magentoCurrent))
+pagesPath = File.expand_path(File.join(themePath, 'pages'))
+blocsPath = File.expand_path(File.join(wwwroot, 'blocs'))
+
 
 FileUtils.cp_r File.join(themePath, appPath), targetPath
 FileUtils.cp_r File.join(skinPath, appPath), targetPath
+
+
+data = YAML.load_file( File.join(blocsPath, 'left_info.yml' )
+puts data.inspect
