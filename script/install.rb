51c43b269e66dd9af5d4f64ba7498f993c2a5efd
Gilles Crofils (Guignes) <gilles@secondbureau.com>
2010-07-25 20:56:23 +0200

    bugfix

--- script/install.rb
+++ script/install.rb
@@ -96,6 +96,7 @@ queries << "update core_cache_option set value=0"
 # emails
 # Load Commons vars
 emailVars = YAML.load_file(File.join(emailsPath, 'common.yml'))
+emailVa
 emailVarsPattern = "/(<<#{emailVars.values.strip.join('|')}>>)/" 
 Dir["#{emailsPath}/*"].select { |file| /(template\.yml)$/ =~ file }.each do |file|
   identifier = File.basename(file, "_template.yml")
