7559f97af6d0dce820539c4e527477186d06d659
Gilles Crofils (Guignes) <gilles@secondbureau.com>
2010-07-28 22:43:11 +0200

    add new attributes management

--- script/load_new_attributes.rb
+++ script/load_new_attributes.rb
@@ -17,6 +17,7 @@ ActiveRecord::Base.establish_connection(
   :host     => 'localhost')
 
 ActiveRecord::Base.logger = Logger.new(File.open('/home/ubuntu/database.log', 'a'))
+logger = ActiveRecord::Base.logger
 
 ActiveRecord::Base.pluralize_table_names = false
 
@@ -27,35 +28,60 @@ def loadDatas(uri)
  begin
   CSV.parse(open(uri).read)
  rescue
+ puts "WARNING : no data"
+
   Array.new
  end
 end
 
 firstrow = true
+attributes = Array.new
+unknown_options = {}
+j=0
 loadDatas(uri_attributes).each do |row|
   if firstrow
     attributes = row 
     firstrow = false
   else
-    sku = row.shift
+    j+=1
+    sku = row[0] 
+    puts "#{j} == #{sku}"
     product = CatalogProductEntity.find_by_sku(sku)
     unless product.nil?
-      (1..row.count).each do |i|
-        eavAttribute = EavAttribute.find_by_attribute_code(attributes[i])
-        case eavAttribute.backend_type
-          when 'varchar'
-            CatalogProductEntityVarchar.create(:entity_type_id => 4, :attribute_id => eavAttribute.id, :store_id => 0, :entity_id => product.entity_id, :value => row[i])
-          when 'int'
-            option = EavAttributeOptionValue.find_by_value(row[i])
-            if option.nil
-              puts "option #{row[i]} inconnue"
-            else
-              CatalogProductEntityInt.create(:entity_type_id => 4, :attribute_id => eavAttribute.id, :store_id => 0, :entity_id => product.entity_id, :value => option.id)
-            end
+      (1..row.size).each do |i|
+        row[i] = row[i].strip
+	unless row[i].nil?		
+          eavAttribute = EavAttribute.find_by_attribute_code(attributes[i])
+          unless eavAttribute.nil? 
+            case eavAttribute.backend_type
+              when 'varchar'
+                begin
+                  CatalogProductEntityVarchar.create(:entity_type_id => 4, :attribute_id => eavAttribute.id, :store_id => 0, :entity_id => product.entity_id, :value => row[i])
+                rescue Exception => e
+                  puts "WARNING : #{e.message}"
+                end
+              when 'int'
+                option = EavAttributeOptionValue.find_by_value(row[i])
+                if option.nil?
+                  puts "WARNING : option #{row[i]} inconnue pour l'attribut #{attributes[i]}"
+		  unknown_options[attributes[i]] = Array.new if unknown_options[attributes[i]].nil? 
+                  unknown_options[attributes[i]] << row[i]	
+                else
+                  begin
+                    CatalogProductEntityInt.create(:entity_type_id => 4, :attribute_id => eavAttribute.id, :store_id => 0, :entity_id => product.entity_id, :value => option.id)
+		  rescue Exception => e
+                    puts "WARNING : #{e.message}"
+                  end
+                end
+	      else
+                puts "WARNING : type inconnu #{eavAttribute.backend_type}"
+              end
+          else
+	    puts "WARNING : >#{attributes[i]}< not found"
+          end
         end
       end
     end
   end
 end
-  
 
